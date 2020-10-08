5bee697fd55975dd56c89f60213979846b608a23
joshuayannix <joshuayannix@gmail.com>
2020-10-08 01:20:44 -0400

    router

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCh0VLCoWJsOvG6Mx6AiLfZfW_uR63F580",
+  authDomain: "fir-ab8e3.firebaseapp.com",
+  databaseURL: "https://fir-ab8e3.firebaseio.com",
+  projectId: "fir-ab8e3",
+  storageBucket: "fir-ab8e3.appspot.com",
+  messagingSenderId: "13782853398",
+  appId: "1:13782853398:web:130df6f8b59fc254235237",
+  measurementId: "G-46BJB1YRSL"
+};

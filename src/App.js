6b2eb06a025b27033f924728b696c9ec08956de8
joshuayannix <joshuayannix@gmail.com>
2020-10-08 23:50:51 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
@@ -8,12 +9,15 @@ function App() {
       <div className="app">
         <Switch>
           <Route path='/checkout'>
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path='/login'>
+            <Header />
             <h1>Login page</h1>
           </Route>
           <Route path='/'>
+            <Header />
             <h1>home page</h1>
           </Route>
         </Switch>

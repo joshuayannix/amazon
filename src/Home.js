1f57b2576020243f1483164a56356d9f01fb14bd
joshuayannix <joshuayannix@gmail.com>
2020-10-09 20:50:57 -0400

    fixed index.css margin

--- src/Home.js
+++ src/Home.js
@@ -13,18 +13,18 @@ function Home() {
       />
       <div className="home__row">
         <Product 
-          id='123123'
-          title='Super Smash Bros. Ultimate - Nintendo Switch'
-          price={59.99}
-          rating={5}
-          image='https://images-na.ssl-images-amazon.com/images/I/81aJ-R4E6gL._SL1500_.jpg'
+         id="12321341"
+         title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+         price={11.96}
+         rating={5}
+         image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
         />
         <Product 
-          id='123123'
-          title='Super Smash Bros. Ultimate - Nintendo Switch'
-          price={59.99}
-          rating={5}
-          image='https://images-na.ssl-images-amazon.com/images/I/81aJ-R4E6gL._SL1500_.jpg'
+          id="49538094"
+          title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+          price={239.0}
+          rating={4}
+          image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
         />
       </div>
       <div className="home__row">
--- src/Product.js
+++ src/Product.js
@@ -2,25 +2,45 @@ import React from 'react'
 import './Product.css'
 
 function Product({ id, title, image, price, rating }) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className='product'>
-      <p>{title}</p>
-      <p className='product__info'>
-        <small>$</small>
-        <strong>{price}</strong>        
-      </p>
-      <div className="product__rating">
-        {
-          Array(rating).fill().map((_) => (
-            <p>⭐️</p>
-          ))
-        }
+      <div className='product__info'>
+        <p>{title}</p>
+        <p className='product__price'>
+          <small>$</small>
+          <strong>{price}</strong>        
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>⭐️</p>
+            ))}
+        </div>
       </div>
 
       <img src={image} alt={title} />
+
       <button>Add to Cart</button>
-    </div>
-  )
+    </div>          
+  );
 }
 
 export default Product
